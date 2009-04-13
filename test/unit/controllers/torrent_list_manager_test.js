b11db2871ededc518b375da0b2b857a346a5dda6
Duncan Beevers <duncanbeevers@gmail.com>
2009-04-13 13:51:34 -0700

    Moving test-specification to pure-js

---
+++ test/unit/controllers/torrent_list_manager_test.js
@@ -0,0 +1,35 @@
+function() { return {
+  // replace this with your real tests
+  setup: function() {
+    
+  },
+  
+  teardown: function() {
+    
+  },
+  testGetIds: function() {
+    var tlm = new Transmission.TorrentListManager();
+    this.assertEnumEqual([ ], tlm.getIds());
+  },
+  
+  testAddIds: function() {
+    var tlm = new Transmission.TorrentListManager();
+    tlm.addIds([ 1 ]);
+    this.assertSameElements([ 1 ], tlm.getIds());
+  },
+  
+  testRemoveIds: function() {
+    var tlm = new Transmission.TorrentListManager();
+    tlm.addIds([ 1, 2 ]);
+    tlm.removeIds([ 1 ]);
+    this.assertSameElements([ 2 ], tlm.getIds());
+  },
+  
+  testNewIdsInList: function() {
+    var tlm = new Transmission.TorrentListManager();
+    tlm.addIds([ 1 ]);
+    var all_torrent_ids = [ 1, 2 ];
+    this.assertSameElements([ 2 ], tlm.newIdsInList(all_torrent_ids));
+  }
+  
+}; }
