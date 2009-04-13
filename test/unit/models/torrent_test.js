e356ebcd24993ab927c01fbe1fcc4aa56405c606
Duncan Beevers <duncanbeevers@gmail.com>
2009-04-13 13:56:46 -0700

    Remove old html-based test files. Replace with javascript based test functions

---
+++ test/unit/models/torrent_test.js
@@ -0,0 +1,15 @@
+function() { return {
+  // replace this with your real tests
+  setup: function() {
+    
+  },
+  
+  teardown: function() {
+    
+  },
+  
+  testTruth: function() {
+    assert(true);
+  }
+  
+}; }
