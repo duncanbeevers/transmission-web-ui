8a86aa34657d4b91d52dfa03d2308251fef0943b
Duncan Beevers <duncanbeevers@gmail.com>
2009-04-27 01:12:55 -0700

    Change event getType method and add a little documentation to event system

--- src/models/events.js
+++ src/models/events.js
@@ -1,22 +1,40 @@
 //= require <transmission>
 
+/**
+  When a class needs to register event listeners it returns
+  an object which protypically-extends EventDispatcher,
+  allowing callbacks for specific event types to be registered
+  on the instance.
+  
+  Event dispatch itself should be invoked within the instance.
+**/
 Transmission.EventDispatcher = (function() {
   var callbacks = {};
   
   return {
     dispatchEvent: function(event) {
-      (callbacks[event.type()] || []).forEach(function(callback) {
+      (callbacks[event.getType()] || []).forEach(function(callback) {
         callback(event);
       });
     },
     addEventListener: function(event_type, callback) {
-      var t = event_type.type();
+      var t = event_type.getType();
       if (!callbacks[t]) { callbacks[t] = []; }
       callbacks[t].push(callback);
     }
   };
 } ());
 
+/**
+  Conveniently construct a namespace of events
+  
+    Transmission.TorrentEvents = Transmission.Events('TorrentAdded', 'TorrentRemoved');
+    event = new Transmission.TorrentEvents.TorrentAdded('data');
+    
+    event.getData() -> 'data'
+    event.getType() -> 'TorrentAdded'
+    
+**/
 Transmission.Events = (function() {
   var o = {};
   for (var i = arguments.length - 1; i >= 0; i--) {
@@ -25,14 +43,20 @@ Transmission.Events = (function() {
   return o;
 });
 
+/**
+  An EventType constructor which encapsulates the name of the type
+  and makes it available through a method on the created EventType
+  itself as well as its instances.
+  
+**/
 Transmission.EventType = (function() { return function(type) {
   var c = function(data) {
     return {
-      type: function() { return type; },
+      getType: function() { return type; },
       getData: function() { return data; }
     }
   };
-  c.type = function() { return type; }
+  c.getType = function() { return type; }
   
   return c;
 }; }());
