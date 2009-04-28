cfbae8d7c76a27ff8b509207fc4eae0659c496bd
Duncan Beevers <duncanbeevers@gmail.com>
2009-04-28 09:04:58 -0700

    Initialize Transmission.WebUI with injected Remote to facilitate mocking

--- src/controllers/web_ui.js
+++ src/controllers/web_ui.js
@@ -1,18 +1,41 @@
 //= require <transmission>
+//= require <torrent_list_manager>
+//= require <remote>
+//= require <timer>
 
 /* The WebUI is the application ojbect responsible for instantiating
     and coordinating other view components.
 */
-Transmission.WebUI = (function() { return function() {
-  var torrents = [];
+Transmission.WebUI = (function() { return function(remote) {
+  var torrent_list_manager = new Transmission.TorrentListManager();
+  
+  var addSliceOfIds = function(ids) {
+    torrent_list_manager.addIds(
+      torrent_list_manager.newIdsInList(ids).slice(0, 10)
+    );
+  };
+  var updateAllIds = function() {
+    remote.requestAllTorrentIds(addSliceOfIds);
+  };
+  
+  var torrent_list_updater = new Transmission.Timer(updateAllIds, <%= TORRENT_IDS_POLL_INTERVAL %>);
+  
+  var start = function() {
+    torrent_list_updater.start();
+  }
+  
+  var markupReady = function() {
+    if (remote) {
+      start();
+    }
+  };
+  
+  var getTorrent = function(id) {
+    return torrent_list_manager.getTorrent(id);
+  };
   
   return {
-    getTorrents: function() {
-      return torrents;
-    },
-    
-    markupReady: function() {
-      
-    }
+    markupReady: markupReady,
+    getTorrent: getTorrent
   };
 }; }());
