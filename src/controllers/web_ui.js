b215fef95e274a734748cfaee0e5c07d91a008b1
Duncan Beevers <duncanbeevers@gmail.com>
2009-05-02 13:34:58 -0700

    Update ScriptLoader in index.html. WebUI initializes with Config and Remote.  Test processing torrents by slice.

--- src/controllers/web_ui.js
+++ src/controllers/web_ui.js
@@ -1,33 +1,35 @@
 //= require <transmission>
 //= require <torrent_list_manager>
+//= require <config>
 //= require <remote>
 //= require <timer>
 
 /* The WebUI is the application ojbect responsible for instantiating
     and coordinating other view components.
 */
-Transmission.WebUI = (function() { return function(remote) {
+Transmission.WebUI = (function() { return function(config, remote) {
   var torrent_list_manager = new Transmission.TorrentListManager();
   
-  var addSliceOfIds = function(ids) {
-    torrent_list_manager.addIds(
-      torrent_list_manager.newIdsInList(ids).slice(0, 10)
-    );
-  };
+  var ids_to_process = [];
   var updateAllIds = function() {
-    remote.requestAllTorrentIds(addSliceOfIds);
+    remote.addEventListener(Transmission.RemoteEvent.RequestAllTorrentIds,
+      function(event) {
+        ids_to_process = ids_to_process.concat(event.getData().ids);
+        torrent_list_updater.start();
+      });
+    remote.requestAllTorrentIds();
+  };
+  var addSliceOfIds = function() {
+    var ids = ids_to_process.splice(0, config.TORRENT_IDS_TO_ADD_PER_PROCESS_INTERVAL),
+        new_ids = torrent_list_manager.newIdsInList(ids);
+    
+    if (new_ids.length) { torrent_list_manager.addIds(new_ids); }
   };
   
-  var torrent_list_updater = new Transmission.Timer(updateAllIds, <%= TORRENT_IDS_POLL_INTERVAL %>);
+  var torrent_list_updater = new Transmission.Timer(addSliceOfIds, config.TORRENT_IDS_PROCESS_INTERVAL);
   
   var start = function() {
-    torrent_list_updater.start();
-  }
-  
-  var markupReady = function() {
-    if (remote) {
-      start();
-    }
+    updateAllIds();
   };
   
   var getTorrent = function(id) {
@@ -35,7 +37,8 @@ Transmission.WebUI = (function() { return function(remote) {
   };
   
   return {
-    markupReady: markupReady,
+    markupReady: start,
+    start: start,
     getTorrent: getTorrent
   };
 }; }());
--- test/unit/controllers/web_ui_test.js
+++ test/unit/controllers/web_ui_test.js
@@ -8,7 +8,7 @@ function() { return {
   },
   
   testMarkupReady: function() {
-    var ui = new Transmission.WebUI();
+    var ui = new Transmission.WebUI(new Transmission.Config(), new Transmission.Remote());
     this.assertNothingRaised(function() {
       ui.markupReady();
     });
@@ -16,8 +16,32 @@ function() { return {
   
   testRequestsIds: function() {
     var remote = new MockRemote();
-    var ui = new Transmission.WebUI(remote);
-    this.assert(remote.haveAllIdsBeenRequested);
+    var ui = new Transmission.WebUI(new Transmission.Config(), remote);
+    ui.start();
+    this.wait(10, function() {
+      this.assert(remote.haveAllIdsBeenRequested());
+    });
+  },
+  
+  testProcessesSlicesOfIds: function() {
+    var remote = new Transmission.Remote();
+    var ui = new Transmission.WebUI(new Transmission.Config({
+      'TORRENT_IDS_PROCESS_INTERVAL': 10000,
+      'TORRENT_IDS_TO_ADD_PER_PROCESS_INTERVAL': 2
+    }), remote);
+    
+    ui.start();
+    
+    this.wait(10, function() {
+      remote.dispatchEvent(
+        new Transmission.RemoteEvent.RequestAllTorrentIds( { ids: [ 1, 2, 3 ] } )
+      );
+      this.wait(10, function() {
+        this.assert(ui.getTorrent(1));
+        this.assert(ui.getTorrent(2));
+        this.assertUndefined(ui.getTorrent(3));
+      });
+    });
   }
   
 }; }
