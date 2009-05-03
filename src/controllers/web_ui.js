209b6708bb589d1aeca7d5796bbe268dd9c6355f
Duncan Beevers <duncanbeevers@gmail.com>
2009-05-03 14:00:34 -0700

    WebUI registers event callback on bootstrapper to feed torrent list manager.

--- src/controllers/web_ui.js
+++ src/controllers/web_ui.js
@@ -1,6 +1,7 @@
 //= require <transmission>
 //= require <torrent_list_manager>
 //= require <torrent_list_bootstrapper>
+//= require <torrent_file_list>
 //= require <config>
 //= require <remote>
 
@@ -9,11 +10,27 @@
 */
 Transmission.WebUI = (function() { return function(config, remote) {
   var torrent_list_manager = new Transmission.TorrentListManager();
+  
+  // Bootstrap the torrent list manager with the list of torrents
+  // known to the server
   var bootstrapper = new Transmission.TorrentListBootstrapper(
     config.TORRENT_IDS_TO_ADD_PER_PROCESS_INTERVAL,
     config.TORRENT_IDS_PROCESS_INTERVAL,
-    torrent_list_manager.addIds,
     remote);
+  bootstrapper.addEventListener(Transmission.TorrentListBootstrapperEvent,
+    function(event) {
+      torrent_list_manager.addIds(event.getData().ids);
+    });
+  
+  // View Controllers
+  var torrent_file_lists = {};
+  torrent_list_manager.addEventListener(
+    Transmission.TorrentListEvent.TorrentAdded,
+    function(event) {
+      var torrent = event.getData().torrent;
+      torrent_file_lists[ torrent.getId() ] = new Transmission.TorrentFileList(torrent, remote);
+    }
+  );
   
   var start = function() {
     bootstrapper.start();
