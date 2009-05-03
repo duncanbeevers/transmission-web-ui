568f6ddcd32593c3113e27ae3f3091854cf2ee93
Duncan Beevers <duncanbeevers@gmail.com>
2009-05-03 16:52:03 -0700

    Torrents can track own attributes.

--- src/models/torrent.js
+++ src/models/torrent.js
@@ -3,11 +3,22 @@
 
 Transmission.Torrent = (function() { return function(id) {
   var files = [];
+  var attributes = {};
+  
   var getId = function() { return id; };
   var getFiles = function() { return files; };
   
+  var updateAttributes = function(new_attributes) {
+    attributes = Object.extend(attributes, new_attributes);
+  };
+  var getAttribute = function(attribute) {
+    return attributes[attribute];
+  };
+  
   return {
     getId: getId,
-    getFiles: getFiles
+    getFiles: getFiles,
+    updateAttributes: updateAttributes,
+    getAttribute: getAttribute
   };
 }; })();
--- test/unit/models/torrent_test.js
+++ test/unit/models/torrent_test.js
@@ -16,6 +16,14 @@ function() { return {
   testGetFiles: function() {
     var torrent = new Transmission.Torrent(1);
     this.assertSameElements([], torrent.getFiles());
+  },
+  
+  testUpdateAttributes: function() {
+    var torrent = new Transmission.Torrent(1);
+    torrent.updateAttributes({
+      hashString: 'b126e1ea1b49c79613f779ac0f36a9714e823fcb'
+    });
+    this.assertEqual('b126e1ea1b49c79613f779ac0f36a9714e823fcb', torrent.getAttribute('hashString'));
   }
   
 }; }
