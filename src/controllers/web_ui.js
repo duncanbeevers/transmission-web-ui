57e7d88fed0d3884487a10dc8de39d649a4f5446
Duncan Beevers <duncanbeevers@gmail.com>
2009-05-02 17:22:19 -0700

    Stop testing bootstrapping functionality in web ui.

--- src/controllers/web_ui.js
+++ src/controllers/web_ui.js
@@ -1,39 +1,22 @@
 //= require <transmission>
 //= require <torrent_list_manager>
+//= require <torrent_list_bootstrapper>
 //= require <config>
 //= require <remote>
-//= require <timer>
 
 /* The WebUI is the application ojbect responsible for instantiating
     and coordinating other view components.
 */
 Transmission.WebUI = (function() { return function(config, remote) {
   var torrent_list_manager = new Transmission.TorrentListManager();
-  
-  var ids_to_process = [];
-  var updateAllIds = function() {
-    remote.addEventListener(Transmission.RemoteEvent.ReceivedAllTorrentIds,
-      function(event) {
-        ids_to_process = ids_to_process.concat(event.getData().ids);
-        torrent_list_updater.start();
-      });
-    remote.requestAllTorrentIds();
-  };
-  var addSliceOfIds = function() {
-    var ids = ids_to_process.splice(0, config.TORRENT_IDS_TO_ADD_PER_PROCESS_INTERVAL),
-        new_ids = torrent_list_manager.newIdsInList(ids);
-    
-    if (new_ids.length) {
-      torrent_list_manager.addIds(new_ids);
-    } else {
-      torrent_list_updater.stop();
-    }
-  };
-  
-  var torrent_list_updater = new Transmission.Timer(addSliceOfIds, config.TORRENT_IDS_PROCESS_INTERVAL);
+  var bootstrapper = new Transmission.TorrentListBootstrapper(
+    config.TORRENT_IDS_TO_ADD_PER_PROCESS_INTERVAL,
+    config.TORRENT_IDS_PROCESS_INTERVAL,
+    torrent_list_manager.addNewIds,
+    remote);
   
   var start = function() {
-    updateAllIds();
+    bootstrapper.start();
   };
   
   var getTorrent = function(id) {
--- test/unit/controllers/web_ui_test.js
+++ test/unit/controllers/web_ui_test.js
@@ -12,41 +12,6 @@ function() { return {
     this.assertNothingRaised(function() {
       ui.markupReady();
     });
-  },
-  
-  testRequestsIds: function() {
-    var remote = new Transmission.Remote();
-    var ui = new Transmission.WebUI(new Transmission.Config(), remote);
-    
-    var have_all_ids_been_requested = false;
-    remote.addEventListener(Transmission.RemoteEvent.RequestedAllTorrentIds, function() {
-      have_all_ids_been_requested = true;
-    });
-    
-    ui.start();
-    this.wait(10, function() {
-      this.assert(have_all_ids_been_requested);
-    });
-  },
-  
-  testProcessesSlicesOfIds: function() {
-    var remote = new Transmission.Remote();
-    var ui = new Transmission.WebUI(new Transmission.Config({
-      'TORRENT_IDS_PROCESS_INTERVAL': 10000,
-      'TORRENT_IDS_TO_ADD_PER_PROCESS_INTERVAL': 2
-    }), remote);
-    
-    ui.start();
-    this.wait(10, function() {
-      remote.dispatchEvent(
-        new Transmission.RemoteEvent.ReceivedAllTorrentIds( { ids: [ 1, 2, 3 ] } )
-      );
-      this.wait(10, function() {
-        this.assert(ui.getTorrent(1));
-        this.assert(ui.getTorrent(2));
-        this.assertUndefined(ui.getTorrent(3));
-      });
-    });
   }
   
 }; }
