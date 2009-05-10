caaa5bed29cb03fb2d7e278250796660427bfa9c
Duncan Beevers <duncanbeevers@gmail.com>
2009-05-09 20:29:32 -0700

    Stop using instance variables for jsunittest server, use locals instead.  Provide links to other tests in suite.

--- lib/spocks_brain/jsunittest_builder_server.rb
+++ lib/spocks_brain/jsunittest_builder_server.rb
@@ -1,25 +1,49 @@
 require 'rubygems'
 require 'sinatra'
+require 'pathname'
 
 class JsUnitTestBuilderServer < Sinatra::Default
-  if !(defined?(APP_ROOT))
+  if !(defined?(APP_CONSTS))
+    APP_CONSTS = true
+    
     APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '../..'))
-  end
-  if !(defined?(TEST_ROOT))
     TEST_ROOT = File.join(APP_ROOT, 'test/unit')
-  end
-  if !(defined?(TEST_LIB))
+    TEST_ROOT_PATHNAME = Pathname.new(TEST_ROOT)
     TEST_LIB = File.join(APP_ROOT, 'test/lib')
   end
+  
   set :public, TEST_LIB
   
   get '/test/*' do
-    @object_under_test = params[:splat]
-    @object_under_test_file_name = "#{@object_under_test}.js"
-    @test_file_name = "#{@object_under_test}_test.js"
-    @test_log = "#{@object_under_test}_log"
-    @function_returning_test_methods = File.read(File.join(TEST_ROOT, @test_file_name))
-    haml :test, :layout => :test_layout
+    object_under_test               = params[:splat]
+    object_under_test_file_name     = "#{object_under_test}.js"
+    test_file_name                  = "#{object_under_test}_test.js"
+    test_log                        = "#{object_under_test}_log"
+    function_returning_test_methods = File.read(File.join(TEST_ROOT, test_file_name))
+    
+    to_test_path = Proc.new do |filename|
+      trunc = Pathname.new(filename).relative_path_from(TEST_ROOT_PATHNAME).to_s.gsub(/\_test.js$/, '')
+      href = '/test/' + trunc
+      name = File.basename(trunc)
+      { :href => href, :name => name }
+    end
+    
+    model_tests                     = Dir[File.join(TEST_ROOT, 'models/**/*_test.js')].map(&to_test_path)
+    controller_tests                = Dir[File.join(TEST_ROOT, 'controllers/**/*_test.js')].map(&to_test_path)
+    
+    haml :test,
+      :layout => :test_layout,
+      :locals => {
+        :object_under_test               => object_under_test,
+        :object_under_test_file_name     => object_under_test_file_name,
+        :test_file_name                  => test_file_name,
+        :test_log                        => test_log,
+        :function_returning_test_methods => function_returning_test_methods,
+        
+        # Navigation
+        :model_tests                     => model_tests,
+        :controller_tests                => controller_tests
+      }
   end
   
   # Helper methods
