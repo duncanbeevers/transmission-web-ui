3438338b5bb53510e3a66f91a008b699c9951f93
Duncan Beevers <duncanbeevers@gmail.com>
2009-06-22 09:50:21 -0700

    Extracted attribute-observing functionality from TorrentFile

--- src/models/torrent_file.js
+++ src/models/torrent_file.js
@@ -7,59 +7,13 @@ Transmission.TorrentFile = (function() {
     return this.torrent;
   };
   
-  var updateAttributes = function(new_attributes) {
-    var new_attribute_value,
-        callbacks = [];
-    
-    for (var attribute in new_attributes) {
-      new_attribute_value = new_attributes[attribute];
-      if (this.attributes[attribute] != new_attribute_value) {
-        this.attributes[attribute] = new_attribute_value;
-        if (this.attribute_callbacks[attribute]) {
-          this.attribute_callbacks[attribute].each(function(callback) {
-            if (!callbacks.include(callback)) {
-              callbacks.push(callback);
-            }
-          });
-        }
-      }
-    }
-    
-    callbacks.each(function(callback) { callback(); });
-  };
-  
-  var addAttributeEventListener = function(attribute, callback) {
-    if (!this.attribute_callbacks[attribute]) {
-      this.attribute_callbacks[attribute] = [];
-    }
-    this.attribute_callbacks[attribute].push(callback);
-  };
-  
-  var addAttributesEventListener = function(attributes, callback) {
-    for (var i = attributes.length - 1; i >= 0; i--) {
-      this.addAttributeEventListener(attributes[i], callback);
-    }
-  };
-  
-  var getAttribute = function(attribute) {
-    return this.attributes[attribute];
-  };
-  
   var constructor = function(torrent) {
     this.torrent = torrent;
-    this.attributes = {};
-    
-    this.attribute_callbacks = {};
   };
   
-  constructor.prototype = {
-    getTorrent: getTorrent,
-    updateAttributes: updateAttributes,
-    getAttribute: getAttribute,
-    
-    addAttributeEventListener: addAttributeEventListener,
-    addAttributesEventListener: addAttributesEventListener
-  };
+  constructor.prototype = Transmission.extend(new Transmission.AttributeEventDispatcher(), {
+    getTorrent: getTorrent
+  });
   
   return constructor;
   
--- test/unit/models/torrent_file_test.js
+++ test/unit/models/torrent_file_test.js
@@ -7,33 +7,10 @@ function() { return {
     
   },
   
-  testAttributes: function() {
-    var torrent = new Transmission.Torrent(),
-        torrent_file = new Transmission.TorrentFile(torrent);
-    torrent_file.updateAttributes({ name: 'Filename' });
-    this.assertEqual('Filename', torrent_file.getAttribute('name'));
-  },
-  
   testGetTorrent: function() {
     var torrent = new Transmission.Torrent(),
         torrent_file = new Transmission.TorrentFile(torrent);
     this.assertEqual(torrent, torrent_file.getTorrent());
-  },
-  
-  testAddAttributeEventListener: function() {
-    var torrent = new Transmission.Torrent(),
-        torrent_file = new Transmission.TorrentFile(torrent),
-        bytes_completed_updated;
-    
-    torrent_file.addAttributeEventListener('bytesCompleted', function() {
-      bytes_completed_updated = true;
-    });
-    
-    torrent_file.updateAttributes({
-      bytesCompleted: 1000
-    });
-    
-    this.assert(bytes_completed_updated);
   }
   
 }; }
