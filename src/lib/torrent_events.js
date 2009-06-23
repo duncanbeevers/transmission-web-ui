0618812cfd8d8ed0e03ffabd8f277d013423465b
Duncan Beevers <duncanbeevers@gmail.com>
2009-06-23 00:46:49 -0700

    Implement TorrentEventDispatcher as prototype-extendable class

--- src/lib/torrent_events.js
+++ src/lib/torrent_events.js
@@ -1,50 +1,57 @@
 //= require <transmission>
 //= require <events>
 
-/**
-  When a class needs to register event listeners it returns
-  an object which protypically-extends EventDispatcher,
-  allowing callbacks for specific event types to be registered
-  on the instance.
+Transmission.TorrentEventDispatcher = (function() {
+  var initialize = function(o) {
+    if (!o.torrents_callbacks) { o.torrents_callbacks = {}; }
+  };
   
-  Event dispatch itself should be invoked within the instance.
-**/
-Transmission.TorrentEventDispatcher = (function() { return function() {
-  var torrents_callbacks = {};
-  
-  return Transmission.extend(new Transmission.EventDispatcher(), {
-    dispatchTorrentEvent: function(event) {
-      var data = event.getData().torrent_data,
-          torrent_callbacks = torrents_callbacks[data.id];
-      if (!torrent_callbacks) { return; }
-      
-      var callbacks = torrent_callbacks[event.getType()];
-      if (!callbacks) { return; }
-      
-      for (var i = callbacks.length - 1; i >= 0; i--) {
-        callbacks[i].defer(event);
-      }
-    },
+  var dispatchTorrentEvent = function(event) {
+    initialize(this);
+    var data = event.getData().torrent_data,
+        torrent_callbacks = this.torrents_callbacks[data.id];
+    if (!torrent_callbacks) { return; }
     
-    addTorrentEventListener: function(event_type, torrent_id, callback) {
-      var t = event_type.getType();
-      if (!torrents_callbacks[torrent_id]) {
-        torrents_callbacks[torrent_id] = {};
-      }
-      if (!torrents_callbacks[torrent_id][t]) {
-        torrents_callbacks[torrent_id][t] = [];
-      }
-      torrents_callbacks[torrent_id][t].push(callback);
-    },
+    var callback_groups = this.torrents_callbacks[event.getType()];
+    if (!callback_groups) { return; }
     
-    removeTorrentEventListener: function(event_type, torrent_id, callback) {
-      if (!torrents_callbacks[torrent_id]) { return; }
-      var t = event_type.getType();
-      if (!torrents_callbacks[torrent_id][t]) { return; }
-      torrents_callbacks[torrent_id][t] = torrents_callbacks[torrent_id][t].filter(function(c) {
-        return c != callback;
-      });
+    for (var id in callback_groups) {
+      var callbacks = callback_groups[id];
+      for (var i = 0, len = callbacks.length; i < len; i++) {
+        callbacks[i](event);
+      }
     }
-    
+  };
+  
+  var addTorrentEventListener = function(event_type, torrent_id, callback) {
+    initialize(this);
+    var t = event_type.getType();
+    if (!this.torrents_callbacks[torrent_id]) {
+      this.torrents_callbacks[torrent_id] = {};
+    }
+    if (!this.torrents_callbacks[torrent_id][t]) {
+      this.torrents_callbacks[torrent_id][t] = [];
+    }
+    this.torrents_callbacks[torrent_id][t].push(callback);
+  };
+  
+  var removeTorrentEventListener = function(event_type, torrent_id, callback) {
+    initialize(this);
+    if (!this.torrents_callbacks[torrent_id]) { return; }
+    var t = event_type.getType();
+    if (!this.torrents_callbacks[torrent_id][t]) { return; }
+    this.torrents_callbacks[torrent_id][t] = this.torrents_callbacks[torrent_id][t].filter(function(c) {
+      return c != callback;
+    });
+  };
+  
+  var constructor = function() {};
+  
+  constructor.prototype = Transmission.extend(Transmission.EventDispatcher.prototype, {
+    dispatchTorrentEvent: dispatchTorrentEvent,
+    addTorrentEventListener: addTorrentEventListener,
+    removeTorrentEventListener: removeTorrentEventListener
   });
-}; })();
+  
+  return constructor;
+})();
